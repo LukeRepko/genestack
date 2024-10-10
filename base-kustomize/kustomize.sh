867e69de504201c3bf207be3f24f0448f8c106e3
Luke Repko <luke.repko@rackspace.com>
2024-10-10 17:55:47 -0500

    fix(kustomize): create base dir if not exist
    Some kustomize overlays might not have a named `base` overlay. Trying to
    use kustomization.sh without a `base` directory results in silent error
    while creating `all.yaml`.
    
    While we should standardize creation of a base overlay for each top
    level service we use for kustomize, this should help prevent confusion
    if the directory doesn't exist yet.

--- base-kustomize/kustomize.sh
+++ base-kustomize/kustomize.sh
@@ -2,6 +2,7 @@
 set -e
 KUSTOMIZE_DIR=${1:-$GENESTACK_KUSTOMIZE_ARG}
 pushd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null
+    test -d "${KUSTOMIZE_DIR}"/../base || mkdir "${KUSTOMIZE_DIR}"/../base
     cat <&0 > "${KUSTOMIZE_DIR}"/../base/all.yaml
     kubectl kustomize --reorder='none' "${KUSTOMIZE_DIR}"
 popd &>/dev/null
